324fee1c6d239264c6e9cf6f67e13c4483b3486b
ChampionWin <championwillwin6@outlook.com>
2020-06-21 19:49:39 +0300

    create hook and class counter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './SimpleGame/App';
+import App from './CounterClass/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
